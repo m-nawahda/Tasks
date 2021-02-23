c98bdd6588e07d072cb2623e9cf79e0598c7317d
mohammed <m.o.h.nawahda@gmail.com>
2021-02-23 10:57:16 +0200

    fix code

--- getItems.js
+++ getItems.js
@@ -1,33 +1,33 @@
-function getRooms() {
-
-    var promise = new Promise(function (myResolve) {
-
-        if (document.readyState == "complete") {
-            myResolve()
-        }
-    })
-
-    promise.then(
-        async function () {
-            let response = await fetch("https://api.jsonbin.io/b/60323b2ea3e9f25d023d11f2");
-            if (response.ok) {
-                let data = await response.json();
-                console.log(data)
-                for (x in data) {
-                    document.getElementById('main-content').innerHTML += '<div class="item-container">\
-                    <div class="item">\
-                            <img onerror="error(this)"src="images/' + data[x].url + '.jpg" alt=' + data[x].name + ' />\
-                            <h2 class="item-name">' + data[x].name + '</h2>\
-                            <hr>\
-                            <p>' + data[x].description + '</p>\
-                     </div>\
-                     </div>'
-                }
-            } else {
-                log("Http-Error" + response.status);
-            }
-        }
-    )
+async function getRooms() {
+    try {
+        let response = await fetch("https://api.jsonbin.io/b/60323b2ea3e9f25d023d11f2");
+        let data = await response.json();
+        data.map((room) => {
+            var itemContainer = document.createElement('div');
+            itemContainer.className = "item-container";
+            var item = document.createElement('div');
+            item.className = "item";
+            var image = document.createElement('img');
+            image.src = "images/" + room.url + ".jpg";
+            image.onerror = function () {
+                error(image)
+            };
+            image.alt = room.name;
+            var nameContainer = document.createElement('h2');
+            nameContainer.innerHTML = room.name;
+            var horizontalLine = document.createElement('hr');
+            var descriptionContainer = document.createElement('p');
+            descriptionContainer.innerHTML = room.description;
+            item.appendChild(image);
+            item.appendChild(nameContainer);
+            item.appendChild(horizontalLine);
+            item.appendChild(descriptionContainer);
+            itemContainer.appendChild(item)
+            document.getElementById('main-content').appendChild(itemContainer);
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 function error(obj) {
